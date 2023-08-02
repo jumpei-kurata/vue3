9e24c73fcc32afc7d5ed68f2e94c31d0555d0bad
Jumpei Kurata <se.jumpei114kurata@gmail.com>
2023-08-02 10:49:35 +0900

    learn

--- js/main.js
+++ js/main.js
@@ -5,11 +5,52 @@ const app = Vue.createApp({
       basePrice: 100,
       message:'Hello Vue.js!',
       number: 100,
-      ok: true
+      ok: true,
+      km: 0,
+      m: 0,
+      firstName: "",
+      lastName: "",
+      fullName: "",
+      colors: [
+        {name: "red"},
+        {name: "green"},
+        {name: "blue"}
+      ]
 
       // message1:'Hello <span style="color:red;">Vue.js!!!!!'
     }),
+    // watch: {
+    //   message: function(newValue, oldValue) {
+    //     console.log("new: %s, old: %s", newValue, oldValue)
+    //   }
+    // },
+    watch: {
+      colors: {
+        handler: function(newValue, oldValue) {
+          console.log("update!")
+        },
+        deep: true
+      },
+      // firstName: function(value) {
+      //   this.fullName = value + " " + this.lastName
+      // },
+      // lastName: function(value) {
+      //   this.fullName = this.firstName + " " + value 
+      // },
+      // km: function(value) {
+      //   console.log(value)
+      //   this.km = value
+      //   this.m = value*1000
+      // },
+      // m: function(value) {
+      //   this.km = value / 1000
+      //   this.m = value
+      // }
+    },
     computed: {
+      fullName: function() {
+        return this.firstName + " " + this.lastName
+      },
       computedNumber: function() {
         return Math.random()
       },
@@ -24,17 +65,21 @@ const app = Vue.createApp({
       reversedMessage: function() {
         return this.message.split('').reverse().join('')
       }
-    }, methods: {
-      reversedMessageMethod: function() {
-        return this.message.split('').reverse().join('')
+    }, 
+    methods: {
+      // reversedMessageMethod: function() {
+      //   return this.message.split('').reverse().join('')
     // , methods: {
     //   clickHndler: function(event) {
     //     this.message = this.message.split("").reverse().join("")
     //   }
     // }
-      },
-      methodsNumber: function(){
-        return Math.random()
+      // },
+      // methodsNumber: function(){
+      //   return Math.random()
+      // },
+      onClick: function(event) {
+        this.colors[1].name = "white"
       }
     }
   })
